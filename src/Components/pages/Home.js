97eaa144be7b021ddc98468c282aff7948097948
mituBarua <mitubarua766@gmail.com>
2021-05-10 20:44:58 +0600

    initial commit

---
+++ src/Components/pages/Home.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import '../../App.css';
+import HeroSection from '../HeroSection';
+import Footer from '../Footer';
+import Card from '../Card';
+function Home() {
+    return (
+        <>
+          <HeroSection/> 
+          <Card/> 
+          <Footer/>
+        </>
+    )
+}
+
+export default Home
